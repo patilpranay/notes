49b20f7413aa8149cb26482bb1c927672cbbddfd
Pranay Patil <patilpranay@gmail.com>
2019-04-16 20:21:52 +0000

    Add Spark examples folder including FlatMap example.

---
+++ spark/examples/FlatMapExample.scala
@@ -0,0 +1,33 @@
+import org.apache.spark.sql.SparkSession
+
+object FlatMapExample extends App {
+  // Specify the master: `spark-submit --master spark://localhost:7077
+  val spark = SparkSession
+    .builder()
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val nums = Seq(Seq(1, 2, 3)).toDF("nums")
+
+  println(nums.show())
+
+  val result = nums
+    .as[Seq[Int]]
+    .flatMap { ns =>
+      ns.map { n =>
+        (ns, n)
+      }
+    }.toDF("nums", "num")
+
+  println(result.show())
+
+  // Using for comprehension logic.
+  val ds = for {
+    ns <- nums.as[Seq[Int]]
+    n  <- ns
+  } yield (ns, n)
+  val result2 = ds.toDF("nums", "num")
+
+  println(result2.show())
+}
