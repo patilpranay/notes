0267c4d2fa1c79ff04a3a9c44db483c02f2d5697
Pranay Patil <patilpranay@gmail.com>
2019-04-17 18:26:59 +0000

    Add simple GroupBy example in Scala.

---
+++ spark/examples/GroupByExample.scala
@@ -0,0 +1,20 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object UdfExample extends App {
+  /* https://github.com/jaceklaskowski/spark-workshop/blob/gh-pages/exercises/spark-sql-exercise-Finding-maximum-values-per-group-groupBy.md */
+
+  val spark = SparkSession
+    .builder()
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val nums = spark.range(5).withColumn("group", 'id % 2)
+
+  nums.show
+
+  val df1 = nums.groupBy($"group").agg(max($"id") as "max_id")
+
+  df1.show
+}
