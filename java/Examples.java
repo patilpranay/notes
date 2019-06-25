d3c118c3276bda7c2dad764fa69e75d20d312374
Pranay Patil <patilpranay@gmail.com>
2019-06-25 21:21:05 +0000

    Initial commit for class containing Java examples.

---
+++ java/Examples.java
@@ -0,0 +1,30 @@
+// Don't need to import anything belonging to java.lang.*
+
+public class Examples {
+
+  /**
+   * Retrieve the component type of any array that is passed in.
+   *
+   * @param arr The array whose component type will be returned.
+   * @return The component type of the provided array.
+   * @throws IllegalArgumentException If the provided argument is not an array.
+   */
+  public static void getArrayComponentType(Object arr)
+    throws IllegalArgumentException {
+    Class<?> arrClass = arr.getClass();
+
+    if (arrClass.isArray() != true) {
+      throw new IllegalArgumentException();
+    }
+
+    System.out.println(arrClass.getComponentType());
+  }
+
+  public static void main(String args[]) {
+    //System.out.println("Hello, world!");
+
+    /* Test out the getArrayComponentType function. */
+    int[] test = new int[10];
+    getArrayComponentType(test);
+  }
+}
